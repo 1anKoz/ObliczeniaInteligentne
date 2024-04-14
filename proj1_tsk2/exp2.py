aa2f8a5ce648f3ff0f3348bc6fd2037229f29bce
1anKoz <jankozinski@wp.pl>
2024-04-14 22:41:15 +0200

    Add do_knn function and calculate accuracies

--- proj1_tsk2/exp2.py
+++ proj1_tsk2/exp2.py
@@ -3,17 +3,18 @@ import numpy as np
 from sklearn import cluster
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.inspection import DecisionBoundaryDisplay
+import sklearn
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import load_breast_cancer, load_wine, load_iris
+from sklearn.neural_network import MLPClassifier
 
 import matplotlib.pyplot as plt
 import matplotlib
 from scipy.spatial import Voronoi, voronoi_plot_2d
-from sklearn.inspection import DecisionBoundaryDisplay
 
-import sklearn
-from sklearn.cluster import DBSCAN
-from sklearn.datasets import load_breast_cancer, load_wine, load_iris
-
-from sklearn.neural_network import MLPClassifier
 import pickle
 import os
 import time
@@ -65,6 +66,22 @@ def do_svc(my_kernel, my_c, X_train, y_train):
     model = model.fit(X_train, y_train)
     return model
 
+def do_knn(x_train, y_train, x_test):
+    n_neighbours_arr = []
+    accuracies_arr = []
+    i = 1
+    while ( i <= 15 ):
+        n_neighbours_arr.append(i)
+        knn = KNeighborsClassifier(n_neighbors = i)
+        knn.fit(x_train, y_train)
+        y_pred = knn.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        accuracies_arr.append(accuracy)
+        i += 1
+
+    return n_neighbours_arr, accuracies_arr
+
+
 def test_accuracy(X_test, y_test, model):
     correct = 0
     for k in range(len(X_test)):
@@ -75,3 +92,19 @@ def test_accuracy(X_test, y_test, model):
             correct = correct + 1
 
     return correct/len(X_test)
+
+
+
+if __name__ == "__main__":
+
+    files = ["2_2.csv", "2_3.csv"]
+    for file in files:
+        X, y_true = load(file)
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y_true, test_size=0.2, train_size=0.8, random_state=42)
+
+        # KNN
+        n_neighbours_arr, accuracies_arr = do_knn(X_train, y_train, X_test)
+        print(n_neighbours_arr)
+        print("************")
+        print(accuracies_arr)
+        print("---------------------------")
