9a3d7cc72e4e427cf8d8b46b7866d33b8c4dad60
1anKoz <jankozinski@wp.pl>
2024-05-09 10:35:39 +0200

    Add edge density feture function

--- proj2_tsk1/mnist_encoding_J.py
+++ proj2_tsk1/mnist_encoding_J.py
@@ -3,7 +3,23 @@ import matplotlib.pyplot as plt
 import tensorflow
 from keras.datasets import mnist
 import numpy as np
+import cv2
 
+
+def edge_density(image):
+    # Convert the image to grayscale
+    #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Apply Canny edge detection
+    edges = cv2.Canny(image, 100, 200)  # Adjust thresholds as needed
+    
+    # Calculate edge density
+    height, width = edges.shape
+    total_pixels = height * width
+    edge_pixels = np.count_nonzero(edges)
+    density = edge_pixels / total_pixels
+    
+    return int(density*1000)
 
 def horizontal_symmetry_score(image):
     height, width = image.shape
@@ -18,7 +34,8 @@ def vertical_symmetry_score(image):
     return np.corrcoef(top_half.flatten(), np.flip(bottom_half, axis=0).flatten())[0, 1]
 
 def central_symmetry_score(image):
-    return (horizontal_symmetry_score(image) + vertical_symmetry_score(image)) / 2
+    result = int(((horizontal_symmetry_score(image) + vertical_symmetry_score(image)) / 2)*1000)
+    return result
 
 
 if __name__ == "__main__":
@@ -26,11 +43,12 @@ if __name__ == "__main__":
 
     ctr = 0
 for x in X_train:
-    print(int(horizontal_symmetry_score(x)*1000))
-    print("**********************************")
-    print(int(vertical_symmetry_score(x)*1000))
-    print("----------------------------------")
-    print(int(central_symmetry_score(x)*1000))
+    # print(int(horizontal_symmetry_score(x)*1000))
+    # print("**********************************")
+    # print(int(vertical_symmetry_score(x)*1000))
+    # print("----------------------------------")
+    print(central_symmetry_score(x))
+    #print(edge_density(x))
     print()
     ctr = ctr + 1
     if(ctr == 10): break
